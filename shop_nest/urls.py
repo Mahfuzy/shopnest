ad6092d9000fccd9c44fc16c592b3ebc91a77d68
Mahfuzy <seidumahfuz18@gmail.com>
2025-03-05 02:41:29 +0000

    corrected the api endpoint for swagger json

--- shop_nest/urls.py
+++ shop_nest/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('api/marketplace/', include('marketplace.urls')),
     
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("swagger//json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
